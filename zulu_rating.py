901fa9e024cf06335148ca9758f35d7725ae3192
Ymx1ZWZveDQ <103547750+Ymx1ZWZveDQ@users.noreply.github.com>
2022-04-13 18:08:00 -0500

    Update zulu_rating.py
    -clean up console output
    -logging function added
    -check for valid URL
    -restart function added
    -driver session quit
    removed try exception

--- zulu_rating.py
+++ zulu_rating.py
@@ -1,7 +1,6 @@
 #imports
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import StaleElementReferenceException
 import time
 import re
 import os
@@ -9,9 +8,11 @@ import sys
 
 #Variables
 checkurl = input("Enter URL to scan: ")
-#https://sites.google.com/chromium.org/driver/downloads
 PATH = "C:\chromedriver.exe"
 timestr = time.strftime("%Y%m%d-%H%M%S")
+line = '---------------------------------------'
+invalid = "invalid"
+ZULU = "https://zulu.zscaler.com/"
 
 #Get LocalDir
 from os.path import expanduser
@@ -25,73 +26,82 @@ options.add_argument("disable-gpu")
 driver = webdriver.Chrome(PATH, chrome_options=options)
 
 #Get ZULU
-driver.get("https://zulu.zscaler.com/")
+driver.get(ZULU)
+
+#devtest#
+#checkurl = "test.com"
 
 #Find URL text box and enter text
 inputElement = driver.find_element_by_id("url")
 inputElement.send_keys(checkurl)
 
 #devtest#
-#checkurl = "test.com"
 #inputElement.send_keys("test.com")
 
 #Find Analyze button and click
 submit = driver.find_element_by_css_selector('[value="Analyze"]')
 submit.click()
 
-#exit Script
-def exit(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-    os.system("taskkill /im py.exe")
-
-#add to Clipboard
+#add Variable to Clipboard
 def clipped(text):
     command = 'echo ' + text.strip() + '| clip'
     os.system(command)
 
-#restart Script
+#restart Script with prompt
 def restart():
-    print("restarting",sys.argv)
-    os.execv(sys.executable, ['python'] + sys.argv)
+	askrestart = input("Restart Script? (Y/N): ")
+	if askrestart=='Y' or askrestart=='y' or askrestart=='yes':
+		print("restarting",sys.argv)
+		os.execv(sys.executable, ['python'] + sys.argv)
+	else:
+		print("Exiting...")
 
-#scrub page for Reputation Score and print output
+#Logging output to Desktop
+def logging():
+	with open(home+'\\Desktop\\log.txt', "a") as external_file:
+		add_text = timestr, checkurl, score, isSafe, myURL
+		print(add_text, file=external_file)
+		external_file.close()
+
+#Printing output to console
+def printoutput():
+	print(line)
+	print("---------Zulu URL Risk Analyzer--------")
+	print(line)
+	print(timestr, checkurl, score, isSafe, myURL)
+	print(line)
+	clipped(myURL)
+	print("Reprot URL copied to Clipboard")
+	print(line)
+
+#validate page and fetch Reputation Score using logging()/printoutput()/restart()
 while True:
-	try:
-		test = driver.find_element_by_id("rep-status").text
-		if test == "Completed":
+		InvalidURL = driver.current_url
+		if  invalid in InvalidURL or ZULU == InvalidURL:
+			driver.quit()
+			print(line)
+			print("Invalid URL - " + checkurl)
+			print(line)
+			print("Please make sure that you supplied valid url. (only ipv4 and FQDN)")
+			print(line)
+			restart()
+			break
+		else:
+			rep = driver.find_element_by_id("rep-status").text
+		if rep == "Completed":
 			score = driver.find_element_by_id("rep-score").text
 			score = re.sub('/100', '', score)
+			intScore = int(score)
 			if int(score) >= 60:
 				isSafe = "Fail"
-			else:
+			else:	
 				isSafe = "Pass"
 			myURL = driver.current_url
-			with open(home+'\\Desktop\\log.txt', "a") as external_file:
-		    		add_text = timestr, checkurl, score, isSafe, myURL
-    				print(add_text, file=external_file)
-    				external_file.close()
-			print("---------------------------------------")
-			print("---------Zulu URL Risk Analyzer--------")
-			print("---------------------------------------")
-			print(timestr, checkurl, score, isSafe, myURL)
-			print("---------------------------------------")
-			clipped(myURL)
-			print("Reprot copied to Clipboard")
-			print("---------------------------------------")
-			askrestart = input("Restart Script? (Y/N): ")
-			if askrestart=='Y' or askrestart=='y' or askrestart=='yes':
-				restart()
-			else:
-				print("Exiting...")
-				exit(int(10))
+			logging()
+			printoutput()
+			driver.quit()
+			restart()
 			break
 		else:
 			print("Scanning...")
 			time.sleep(1)
-	except StaleElementReferenceException:
-		continue
